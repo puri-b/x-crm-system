b3a7b030a90e8e371fa5bebe95e7edad6685ca6a
puri-b <puri.b@siamraj.com>
2025-12-02 11:00:59 +0700

    Change: Bug Customers List

--- public/script.js
+++ public/script.js
@@ -1022,7 +1022,7 @@ async function updateCustomer(customerId) {
         email: formData.get('email'),
         lead_source: formData.get('lead_source'),
         sales_person: formData.get('sales_person'),
-        customer_status: formData.get('customer_status')
+        customer_status: formData.get('customer_status'),
         search_keyword: formData.get('search_keyword') ,
         no_quotation_reason: formData.get('no_quotation_reason')
     };
